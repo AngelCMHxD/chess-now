ec26d110ad940f759b8e48928ce620a1c90c5808
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-06-21 22:26:46 -0500

    change Session type

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { deviceAuthorizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
 	plugins: [deviceAuthorizationClient()],
 	baseURL: "http://localhost:8080",
 	basePath: process.env.BETTER_AUTH_PATH,
 });
-export type Session = typeof authClient.$Infer.Session;
+export type Session = typeof auth.$Infer.Session;
