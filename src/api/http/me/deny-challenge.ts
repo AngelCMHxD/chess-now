fa3602381160d37ec0bae03fd4b0e7ddfefeafe5
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-06-22 11:45:14 -0500

    Add /me/challenges/:challenge_id/deny endpoint

---
+++ src/api/http/me/deny-challenge.ts
@@ -0,0 +1,104 @@
+import { and, eq } from "drizzle-orm";
+import z from "zod";
+import { app } from "@/api";
+import { getChallengeInfo } from "@/api/helper";
+import { auth } from "@/lib/auth";
+import { db, schemas } from "@/lib/database";
+
+export const headersType = z.object({
+	authorization: z
+		.string({
+			error: "An 'authorization' header is required",
+		})
+		.startsWith("Bearer ", {
+			error: "'authorization' header must start with 'Bearer '",
+		}),
+});
+
+export async function run(bearer: string, challengeId: string) {
+	const session = await auth.api.getSession({
+		headers: {
+			Authorization: bearer,
+		},
+	});
+
+	if (!session) {
+		return {
+			type: "error",
+			content: {
+				code: 401,
+				error: "Unauthorized",
+			},
+		};
+	}
+
+	if (
+		session.session.scopes &&
+		!session.session.scopes.includes("challenges")
+	)
+		return {
+			type: "error",
+			content: {
+				code: 403,
+				error: "Forbidden",
+			},
+		};
+
+	const cId = parseInt(challengeId, 10);
+
+	if (Number.isNaN(cId))
+		return {
+			type: "error",
+			content: {
+				code: 400,
+				error: "Bad Request",
+			},
+		};
+
+	const challengeInfo = await getChallengeInfo(cId);
+
+	if (!challengeInfo)
+		return {
+			type: "error",
+			content: {
+				code: 404,
+				error: "Challenge Not Found",
+			},
+		};
+
+	if (challengeInfo.to !== session.user.id)
+		return {
+			type: "error",
+			content: {
+				code: 403,
+				error: "Forbidden",
+			},
+		};
+
+	db.update(schemas.challenges)
+		.set({
+			status: "denied",
+		})
+		.where(
+			and(
+				eq(schemas.challenges.id, challengeInfo.id),
+				eq(schemas.challenges.to, session.user.id),
+			),
+		);
+
+	app.server.publish(
+		`challenge:${challengeInfo.from}`,
+		JSON.stringify({
+			type: "challenge:denied",
+			content: {
+				websocketUserId: challengeInfo.from,
+			},
+		}),
+	);
+
+	return {
+		type: "success",
+	};
+}
+
+export default { headersType, run };
--- src/api/index.ts
+++ src/api/index.ts
@@ -12,6 +12,7 @@ import httpDeviceToken from "./http/device/device-token";
 import httpGetMatchInfo from "./http/match/get-match-info";
 import httpMove from "./http/match/move";
 import httpAcceptChallenge from "./http/me/accept-challenge";
+import httpDenyChallenge from "./http/me/deny-challenge";
 import httpGetAccountInfo from "./http/me/get-account-info";
 import httpGetChallenges from "./http/me/get-challenges";
 import httpGetMatches from "./http/me/get-matches";
@@ -92,6 +93,14 @@ export const app = new Elysia({ prefix: "/api", normalize: "typebox" })
 			httpAcceptChallenge.run(headers.authorization, params.challenge_id),
 		{
 			headers: httpAcceptChallenge.headersType,
+		},
+	)
+	.post(
+		"/me/challenges/:challenge_id/deny",
+		({ headers, params }) =>
+			httpDenyChallenge.run(headers.authorization, params.challenge_id),
+		{
+			headers: httpDenyChallenge.headersType,
 		},
 	)
 	.get(
