db63fc1b955a1a81520f9997684ad694d88872da
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-06-24 15:56:27 -0500

    Check for scopes on device approval

--- src/api/http/device/device-approve.ts
+++ src/api/http/device/device-approve.ts
@@ -9,6 +9,19 @@ export const bodyType = z.object({
 export async function run(headers: Headers, body: z.infer<typeof bodyType>) {
 	headers.set("x-internal-call", process.env.INTERNAL_API_SECRET as string);
 
+	const session = await auth.api.getSession({
+		headers,
+	});
+
+	if ((session?.session?.scopes?.length ?? 0) > 0)
+		return {
+			type: "error",
+			content: {
+				code: 403,
+				error: "Forbidden",
+			},
+		};
+
 	const deviceAuth = await auth.api.deviceApprove({
 		body,
 		headers,
