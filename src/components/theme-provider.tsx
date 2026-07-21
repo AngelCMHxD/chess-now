e3707c60df83fba23eb1a5137b2c1889ed1e6331
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-07-20 23:56:23 -0500

    What was this comment doing here? lol

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,5 +7,5 @@ export function ThemeProvider({
 	children,
 	...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>; // a
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
