90909e119134e84d63407b9044a3ab11d77dfbf0
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-07-23 22:40:49 -0500

    Make the user on sidebar a link to the profile

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -98,30 +98,35 @@ export function NavUser({ user, loading }: { user?: User; loading: boolean }) {
 						sideOffset={4}
 					>
 						<DropdownMenuLabel className="p-0 font-normal flex items-center">
-							<div className="flex flex-1 items-center gap-2 px-1 py-1.5 text-left text-sm">
-								<Avatar className="h-8 w-8 rounded-full">
-									<AvatarImage
-										src={user.image}
-										alt={user.name}
-									/>
-									<AvatarFallback className="rounded-full">
-										{(user.name ?? "")
-											.split(" ")
-											.map((n) => n[0])
-											.join("")
-											.slice(0, 2)
-											.toUpperCase() || "CN"}
-									</AvatarFallback>
-								</Avatar>
-								<div className="grid flex-1 text-left text-sm leading-tight">
-									<span className="truncate font-medium">
-										{user.name}
-									</span>
-									<span className="truncate text-xs">
-										@{user.username}
-									</span>
-								</div>
-							</div>
+							<Button variant="ghost" asChild>
+								<Link
+									className="flex flex-1 items-center gap-2 py-1.5 text-left text-sm h-12"
+									href={`/users/${user.username}`}
+								>
+									<Avatar className="h-8 w-8 rounded-full">
+										<AvatarImage
+											src={user.image}
+											alt={user.name}
+										/>
+										<AvatarFallback className="rounded-full">
+											{(user.name ?? "")
+												.split(" ")
+												.map((n) => n[0])
+												.join("")
+												.slice(0, 2)
+												.toUpperCase() || "CN"}
+										</AvatarFallback>
+									</Avatar>
+									<div className="grid flex-1 text-left text-sm leading-tight">
+										<span className="truncate font-medium">
+											{user.name}
+										</span>
+										<span className="truncate text-xs">
+											@{user.username}
+										</span>
+									</div>
+								</Link>
+							</Button>
 							<Button variant="ghost" size="icon-lg" asChild>
 								<Link href="/account/dashboard/settings">
 									<SettingsIcon />
